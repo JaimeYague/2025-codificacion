856f0d60231c1f8cc417d9449a031336e3e8c8d3
unknown <jaimeyaguem@gmail.com>
2025-04-07 11:46:15 +0200

    tabla creada

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+def asciilist():
+    listaascii = list(range(32, 256))
+    qty = 5
+
+    for i in listaascii:
+        number = str(i).ljust(3)
+        caracter = chr(i).ljust(1, "|")
+        print("|", number,"==", caracter, end=" | ")
+        if i % qty == 0:
+            print()
+
 if __name__ == "__main__":
-    print(chr(98))
-    print("=======")
-    print(ord('c'))
+    asciilist()
