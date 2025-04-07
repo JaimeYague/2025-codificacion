6b7b266af68decf7c73af627d38b456d51950911
unknown <jaimeyaguem@gmail.com>
2025-04-07 12:14:46 +0200

    cambios en variables

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def asciilist():
-    listaascii = list(range(32, 256))
-    qty = 5
+    listaascii = list(range(31, 256))
+    columnas = 5
 
     for i in listaascii:
         number = str(i).ljust(3)
         caracter = chr(i).ljust(1, "|")
         print("|", number,"==", caracter, end=" | ")
-        if i % qty == 0:
+        if not (i % columnas):
             print()
 
 if __name__ == "__main__":
