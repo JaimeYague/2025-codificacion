71f0148f3c3c157cba2f5daa839fb0bec873ba57
unknown <jaimeyaguem@gmail.com>
2025-04-10 09:33:46 +0200

    funciones creadas

--- cuadricula.py
+++ cuadricula.py
@@ -1,16 +1,21 @@
 import os
 
-dimensiones = {
-    "col": os.get_terminal_size().columns,
-    "lines": os.get_terminal_size().lines,
-}
 
-def cuadricula():
+def dimensiones():
+    dimensiones = {
+        "col": os.get_terminal_size().columns,
+        "lines": os.get_terminal_size().lines,
+    }
     col = dimensiones["col"]
     lines = dimensiones["lines"]
+
     cont1 = "Contador Izq.: " + str(col)
     cont2 = "Contador Der.: " + str(lines)
-    
+
+    return col, lines, cont1, cont2
+
+
+def cuadricula(col, lines, cont1, cont2):   
     margen = 2
 
     print(cont1 + cont2.rjust(col - len(cont1)), end="")
@@ -20,9 +25,22 @@ def cuadricula():
         print( "║" + ("*" * (col - margen) + "║"), end="")
 
     print("╚" + ("═" * (col - margen)) + "╝", end="")
+
+    return cont1, cont2, col, lines
+
+
+def registros():
     print("Registro 1: ")
     print("Registro 2: ")
     print("Registro 3: ")
 
-cuadricula()
 
+def main():
+    col, lines, cont1, cont2 = dimensiones()
+    cuadricula(col, lines, cont1, cont2)
+    registros()
+
+    
+if __name__ == "__main__":
+
+    main()
